ae18bc011dd1debb48a98af2fc54f3005bdbe067
Werick Nalyson <wericknalyson@gmail.com>
2021-03-18 11:18:05 -0300

    add resize_image in Produto

--- produto/models.py
+++ produto/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+from PIL import Image
+import os
 
 
 class Produto(models.Model):
@@ -20,6 +21,33 @@ class Produto(models.Model):
             ('S', 'Simples')
         )
     )
+
+    @staticmethod
+    def resize_image(img, new_width=800):
+        img_full_path = os.path.join(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+        print('Redimentsionado com sucesso')
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
 
     def __str__(self):
         return self.nome
