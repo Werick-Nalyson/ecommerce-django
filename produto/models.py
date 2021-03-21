b462237596e4f69dba1e8be4f1b4ca5ea783e67d
Werick Nalyson <wericknalyson@gmail.com>
2021-03-20 22:23:38 -0300

    refactor slug produto

--- produto/models.py
+++ produto/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.conf import settings
 from PIL import Image
 import os
+from django.utils.text import slugify
 
 
 class Produto(models.Model):
@@ -10,14 +11,14 @@ class Produto(models.Model):
     descricao_longa = models.TextField()
     imagem = models.ImageField(
         upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     preco_marketing = models.FloatField()
     preco_marketing_promocional = models.FloatField(default=0)
     tipo = models.CharField(
         default='V',
         max_length=1,
         choices=(
-            ('V', 'Variação'),
+            ('V', 'Variável'),
             ('S', 'Simples')
         )
     )
@@ -41,6 +42,10 @@ class Produto(models.Model):
         )
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+
         super().save(*args, **kwargs)
         max_image_size = 800
 
